e4c8854d8a9a7e96abc5c6bd20daea50de38e59c
Joseph <josephylee72@gmail.com>
2023-09-18 18:52:54 -0700

    started intro to context

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Your code here
+function createEmployeeRecord(array) {
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  };
+}
+
+function createEmployeeRecords(array) {
+  let result = [];
+  for (let arr of array) result.push(createEmployeeRecord(arr));
+  return result;
+}
+
+function createTimeInEvent(employeeRecord, dateStamp) {
+  const dates = dateStamp.split(" ")
+  employeeRecord.timeInEvents.push({
+    type: "TimeIn",
+    hour: dates[1],
+    date: dates[0]
+  })
+}
+
+function createTimeOutEvent(employeeRecord, dateStamp) {
+  const dates = dateStamp.split(" ")
+  employeeRecord.timeOutEvents.push({
+    type: "TimeOut",
+    hour: dates[1],
+    date: dates[0]
+  })
+}
+
+function hoursWorkedOnDate(employeeRecord, date) {
+  const index = employeeRecord.timeOutEvents.indexOf(employeeRecord.timeOutEvents.find(obj => obj.date === date));
+  const hoursWorked = +employeeRecord.timeOutEvents[index].hour - +employeeRecord.timeInEvents[index].hour;
+  return hoursWorked;
+}
+
+function wagesEarnedOnDate(employeeRecord, date) {
+
+}
+
+function allWagesFor(employeeRecord) {
+
+}
+
+function calculatePayroll(array) {
+
+}
