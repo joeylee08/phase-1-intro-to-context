fad4cbf477881df1fc38bc1ffbb857ec8e21cb9a
Joseph <josephylee72@gmail.com>
2023-09-18 22:02:30 -0700

    completed intro to context

--- index.js
+++ index.js
@@ -19,34 +19,48 @@ function createTimeInEvent(employeeRecord, dateStamp) {
   const dates = dateStamp.split(" ")
   employeeRecord.timeInEvents.push({
     type: "TimeIn",
-    hour: dates[1],
+    hour: +dates[1],
     date: dates[0]
   })
+  return employeeRecord;
 }
 
 function createTimeOutEvent(employeeRecord, dateStamp) {
   const dates = dateStamp.split(" ")
   employeeRecord.timeOutEvents.push({
     type: "TimeOut",
-    hour: dates[1],
+    hour: +dates[1],
     date: dates[0]
   })
+  return employeeRecord;
 }
 
 function hoursWorkedOnDate(employeeRecord, date) {
-  const index = employeeRecord.timeOutEvents.indexOf(employeeRecord.timeOutEvents.find(obj => obj.date === date));
+  const index = employeeRecord.timeOutEvents
+    .indexOf(employeeRecord.timeOutEvents
+    .find(obj => obj.date === date));
   const hoursWorked = +employeeRecord.timeOutEvents[index].hour - +employeeRecord.timeInEvents[index].hour;
-  return hoursWorked;
+  return hoursWorked / 100;
 }
 
 function wagesEarnedOnDate(employeeRecord, date) {
-
+  const payRate = employeeRecord.payPerHour;
+  const hoursWorked = hoursWorkedOnDate(employeeRecord, date)
+  return payRate * hoursWorked;
 }
 
 function allWagesFor(employeeRecord) {
-
+  let wages = 0;
+  for (let day of employeeRecord.timeInEvents) {
+    wages += wagesEarnedOnDate(employeeRecord, day.date)
+  }
+  return wages;
 }
 
 function calculatePayroll(array) {
-
+  let payroll = 0;
+  for (let employee of array) {
+    payroll += allWagesFor(employee)
+  }
+  return payroll;
 }
